c7433208d2ba20791b854ead19b4a7022df7a695
cineonizer <jamesdkiam@gmail.com>
2021-05-06 21:46:23 -0700

    started the ui,

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@
 import { config } from './Modules/config';
 
 const searchBar = document.querySelector('.search-input');
-const searchBtn = document.querySelector('.search-button');
 
 const fetchWeather = async () => {
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchBar.value}&appid=${config.APIKey}&units=${config.units}`, { mode: 'cors' });
   const weatherData = await response.json();
-  return weatherData.main;
+  console.log(weatherData);
 };
 
-searchBtn.addEventListener('click', fetchWeather);
+searchBar.addEventListener('click', fetchWeather);
